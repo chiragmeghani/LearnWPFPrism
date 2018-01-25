f53d5c50711f499d54b01b2558afd3f95d1c4335
Chirag Meghani <chirag.meghani@gmail.com>
2018-01-25 18:05:44 +0530

    Project is created to explain C# WPF MVVM development using Prism. This project is created for video tutorial - https://youtu.be/D65W7fFsBYA.

---
+++ LearnWPFPrism/Bootstrapper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Prism.Unity;
+using Microsoft.Practices.Unity;
+
+namespace LearnWPFPrism
+{
+    public class Bootstrapper : UnityBootstrapper
+    {
+        public override void Run(bool runWithDefaultConfiguration)
+        {
+            base.Run(runWithDefaultConfiguration);
+        }
+
+        //Create the shell
+        //Show the shell window
+
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<Shell>();
+        }
+
+        protected override void InitializeShell()
+        {
+            //Set main window for Prism
+            App.Current.MainWindow = (Window)Shell;
+            App.Current.MainWindow.Show();
+        }
+
+        protected override void ConfigureModuleCatalog()
+        {
+            base.ConfigureModuleCatalog();//User once we create a module
+
+            Type sampleModuleType = typeof(SampleModule.SampleModule);
+            ModuleCatalog.AddModule(new Prism.Modularity.ModuleInfo { ModuleName = sampleModuleType.Name, ModuleType = sampleModuleType.AssemblyQualifiedName });
+        }
+    }
+}
---
+++ SampleModule/SampleModule.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace SampleModule
+{
+    public class SampleModule : IModule
+    {
+        private IRegionManager _regionManager;
+
+        public SampleModule(IRegionManager regionManager)
+        {
+            _regionManager = regionManager;
+        }
+
+        public void Initialize()
+        {
+            _regionManager.RegisterViewWithRegion("MainRegion", typeof(Views.SampleView));
+        }
+    }
+}
